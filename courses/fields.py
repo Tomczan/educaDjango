a37606c31cab5f02c89b01ea6be034ecc22495a3
Tomasz Czuba <t.czuba95@gmail.com>
2022-03-09 15:51:13 +0100

    add: custom model field 'OrderField'

---
+++ courses/fields.py
@@ -0,0 +1,30 @@
+from mimetypes import init
+from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+
+
+class OrderField(models.PositiveIntegerField):
+    def __init__(self, for_fields=None, *args, **kwargs):
+        self.for_fields = for_fields
+        super().__init__(*args, **kwargs)
+
+    def pre_save(self, model_instance, add):
+        if getattr(model_instance, self.attname) is None:
+            # brak biezacej wartosci
+            try:
+                qs = self.model.objects.all()
+                if self.for_fields:
+                    # filtrowanie wzgledem obiektorw o tych samych wartosciach kolumny
+                    # dla kolumn w "for_fields"
+                    query = {field: getattr(model_instance, field)
+                             for field in self.for_fields}
+                    qs = qs.filter(**query)
+                # pobranie kolejnosci ostatniego elementu
+                last_item = qs.latest(self.attname)
+                value = last_item.order + 1
+            except ObjectDoesNotExist:
+                value = 0
+            setattr(model_instance, self.attname, value)
+            return value
+        else:
+            return super(OrderField, self).pre_save(model_instance, add)
