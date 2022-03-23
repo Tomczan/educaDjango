a56bbc9d0059d9ded33495df648091bfafdcbb40
Tomasz Czuba <t.czuba95@gmail.com>
2022-03-23 16:51:00 +0100

    add additional action in CourseViewSet
    deleted one path in api/urls.py because is no longer needed

--- courses/api/views.py
+++ courses/api/views.py
@@ -4,6 +4,7 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.decorators import action
 from ..models import Subject, Course
 from .serializers import SubjectSerializer, CourseSerializer
 
@@ -31,3 +32,11 @@ class CourseEnrollView(APIView):
 class CourseViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+
+    @action(detail=True,
+            methods=['post'],
+            authentication_classes=[IsAuthenticated])
+    def enroll(self, request, *args, **kwargs):
+        course = self.get_object()
+        course.students.add(request.user)
+        return Response({'enrolled': True})
