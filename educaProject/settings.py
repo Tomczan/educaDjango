ff8f45296851b1bfbd771eef406555b99ecad194
Tomczan <t.czuba95@gmail.com>
2022-03-30 19:36:25 +0200

    configurated communication channel layers using redis

--- educaProject/settings.py
+++ educaProject/settings.py
@@ -169,3 +169,14 @@ REST_FRAMEWORK = {
 # ASGI
 
 ASGI_APPLICATION = 'educaProject.routing.application'
+
+# Channel layers - redis
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'Config': {
+            'hosts': [('127.0.0.1', 6379)],
+        }
+    }
+}
